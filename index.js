b36e06f2d3d4fff27970ed525a80777d74d4f085
Chad <chadg1980@gmail.com>
2018-08-28 11:20:26 -0700

    Added error checking for incoming data and write to S3 bucket

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 var Airtable = require('airtable');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
 var base = new Airtable({apiKey: process.env.AIRTABLE_APIKEY}).base(process.env.AIRTABLE_BASE);
 
 function getRecords(callback){
        
             
-    let products = []
+    let products =[];
     
     base('Products').select({
         // Selecting the first 3 records in Grid view:
@@ -13,17 +15,18 @@ function getRecords(callback){
         view: "Grid view"
     }).eachPage(function page(records, fetchNextPage) {
         // This function (`page`) will get called for each page of records.
-    
+        
         records.forEach(function(record) {
-                
+            if(record.get('product_name')){
                 products.push({
                     'product_name':record.get('product_name'),
                     'ingredients': record.get('ingredients'),
                     'image_filename': record.get('image_filename'),
+                    'alt_text' : record.get('image_alt_text'),
                     'notes': record.get('notes'),
                     'sold_out': record.get('sold_out'), 
                     'deleted': record.get('deleted')  });
-            
+            }
             
         });
     
@@ -34,22 +37,28 @@ function getRecords(callback){
     
     }, function done(err) {
         if (err) {"error" +  callback(err); }
-       callback(null, "Thank You!");
+        let bucketName =  process.env.BUCKET;
+
+        var params = {
+            Body: JSON.stringify(products),
+            Bucket: bucketName,
+            Key: "assets/index.json"
+        }
         
+        
+        s3.putObject(params, function(err, data){
+            if(err) console.log(err, err.stack);
+            else console.log(data);
+            callback(null, data);
+        })
         
     });
 };
-
-
 
 
 exports.handler = (event, context, callback) => {
     console.log("starting...")
     getRecords(callback);
 
-   
-    
-
-    
 }
 
